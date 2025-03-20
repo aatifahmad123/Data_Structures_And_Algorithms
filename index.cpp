c2c8547bbb34cd34379c0893532f085672d92547
aatifahmad123 <b22ai002@iitj.ac.in>
2025-03-21 04:37:56 +0530

    commit zero

---
+++ index.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getProduct(int a,int b){
+
+  return a * b;
+}
+
+
+int main(){
+
+  int a = 10, b = 5;
+  int product = getProduct(a,b);
+  cout << "Product of numbers : " << product << endl;
+
+  return 0;
+} 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int addTwoNumbers(int a, int b){
+
+  return a + b;
+}
+
+
 int main(){
 
   cout << "Hello World" << endl;
+
+  int a = 10, b = 20;
+  int sum = addTwoNumbers(a,b);
+  cout << "The sum of the two numbers are : " << sum << endl;
 
   return 0;
 }
