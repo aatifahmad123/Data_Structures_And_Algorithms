9acd3ee003d49d35504c92aaaac60d1a72c11223
aatifahmad123 <b22ai002@iitj.ac.in>
2025-03-21 20:15:31 +0530

    Added readme

--- index.py
+++
@@ -1,7 +0,0 @@
-import math
-import numpy as np
-
-nums = np.array([1,2,3,4])
-
-print(f'Maximum Number  : {np.max(nums)}')
-
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-
-  cout << "Hello World!, Welcome to Data Structures and Algorithms" << endl;
-
-  return 0;
-}
