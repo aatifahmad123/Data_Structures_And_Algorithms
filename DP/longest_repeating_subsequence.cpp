b19d449cb2022f32c5c1aad0589bf973876d4134
aatifahmad123 <b22ai002@iitj.ac.in>
2025-05-02 14:57:57 +0530

    Add skeleton for longest repeating subsequence implementation

---
+++ DP/longest_repeating_subsequence.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestRepeatingSubsequence(string st, int n){
+	
+}
+
+
+int main() {
+
+    string st = "aabcbdc";
+    int n = st.size();
+    cout << longestRepeatingSubsequence(st,n) << endl;
+
+    return 0;
+}
